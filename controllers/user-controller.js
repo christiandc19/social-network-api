06257f87e6f8ed551cf5ee2fd259377fbc255e0c
unknown <christiandc19@hotmail.com>
2022-04-28 21:13:16 -0700

    fixed add/delete friend error

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -71,27 +71,14 @@ addFriend({ params }, res) {
   User.findOneAndUpdate(
       { _id: params.userId },
       { $addToSet: { friends: params.friendId } },
-      { new: true, runValidators: true }
+      { new: true }
   )
   .then(dbUserData => {
       if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this userId' });
           return;
       }
-      // add userId to friendId's friend list
-      User.findOneAndUpdate(
-          { _id: params.friendId },
-          { $addToSet: { friends: params.userId } },
-          { new: true, runValidators: true }
-      )
-      .then(dbUserData2 => {
-          if(!dbUserData2) {
-              res.status(404).json({ message: 'No user found with this friendId' })
-              return;
-          }
-          res.json(dbUserData);
-      })
-      .catch(err => res.json(err));
+      res.json(dbUserData);
   })
   .catch(err => res.json(err));
 },
@@ -103,27 +90,14 @@ deleteFriend({ params }, res) {
   User.findOneAndUpdate(
       { _id: params.userId },
       { $pull: { friends: params.friendId } },
-      { new: true, runValidators: true }
+      { new: true}
   )
   .then(dbUserData => {
       if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this userId' });
           return;
       }
-      // remove userId from friendId's friend list
-      User.findOneAndUpdate(
-          { _id: params.friendId },
-          { $pull: { friends: params.userId } },
-          { new: true, runValidators: true }
-      )
-      .then(dbUserData2 => {
-          if(!dbUserData2) {
-              res.status(404).json({ message: 'No user found with this friendId' })
-              return;
-          }
-          res.json({message: 'Successfully deleted the friend'});
-      })
-      .catch(err => res.json(err));
+      res.json(dbUserData);
   })
   .catch(err => res.json(err));
 }
